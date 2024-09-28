cb64a33231f73355537e588a000d431c3423308d
yashwanth1403 <yashwanthmittabai@gmail.com>
2024-09-28 09:27:47 +0530

    first commit

--- auth.ts
+++ auth.ts
@@ -1,10 +1,6 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "./lib/db";
-
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },

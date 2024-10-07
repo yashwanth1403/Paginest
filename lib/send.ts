37d97d63d70bae180084f91a19191026648c589e
yashwanth1403 <yashwanthmittabai@gmail.com>
2024-10-07 18:16:21 +0530

    update auth && nodemailer

--- lib/send.ts
+++ lib/send.ts
@@ -5,6 +5,7 @@ const domain = "https://paginest.vercel.app";
 export async function sendVerification(email: string, token: string) {
   try {
     const confirmationLink = `${domain}/verify-email-token?token=${token}`;
+    console.log(confirmationLink);
     const transporter = nodemailer.createTransport({
       service: "gmail", // or use 'smtp.sendgrid.net' for SendGrid
       auth: {

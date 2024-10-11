d8e29d851dedfc57d7b5f38e7ec50bb1b4d7a683
yashwanth1403 <yashwanthmittabai@gmail.com>
2024-10-11 21:19:40 +0530

    Update socialauth

--- app/random/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-const Random = () => {
-  const randomNumber = Math.floor(Math.random() * 1000) + 1;
-
-  return <div>Random number: {randomNumber}</div>;
-};
-
-export default Random;

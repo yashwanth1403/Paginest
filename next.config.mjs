bfc983a789e3c2e9d81e75f6a5a1692589cde7dd
yashwanth1403 <yashwanthmittabai@gmail.com>
2024-10-11 20:50:42 +0530

    Update socialauth

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"], // Add your external image host here
+    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
 };
 

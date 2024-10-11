5e75712cbd2f5bdd1d7687493eb8fe3d9406f9ed
yashwanth1403 <yashwanthmittabai@gmail.com>
2024-10-11 10:33:11 +0530

    Update socialauth

--- app/[userid]/page.tsx
+++ app/[userid]/page.tsx
@@ -6,14 +6,24 @@ import { getUserById } from "@/data/user";
 import Image from "next/image";
 export default async function Page({ params }: { params: { userid: string } }) {
   const user = await getUserById(params.userid);
+  const session = await auth();
   if (!user) {
     return <div>No user found</div>;
   }
-  const session = await auth();
+  if (session?.user.id !== user.id) {
+    return <div>no user found</div>;
+  }
+
   return (
     <div>
-      <div className="text-xl">
-        <Image src={user.image || " "} alt="No avatar"></Image>
+      <div className="text-sm md:text-xl p-6">
+        <Image
+          src={user.image || " "}
+          alt="User profile picture"
+          width={100} // Adjust width as needed
+          height={100} // Adjust height as needed
+          className="rounded-full" // Optional: Add a class for styling
+        />
         welcome <b>{session?.user.name}</b>
         <p>email:{session?.user.email}</p>
       </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com"], // Add your external image host here
+  },
+};
 
 export default nextConfig;

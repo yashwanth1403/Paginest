83b2748f59d2e5c203e57b8f2cc45ae77694cc41
yashwanth1403 <yashwanthmittabai@gmail.com>
2024-10-11 20:56:08 +0530

    Update socialauth

--- app/random/page.tsx
+++ app/random/page.tsx
@@ -1,5 +1,12 @@
+import { useEffect, useState } from "react";
+
 const Random = () => {
-  return <div>Random number:{Math.floor(Math.random() * 1000) + 1}</div>;
+  const [random, setRandom] = useState<number>(0);
+  useEffect(() => {
+    const random = Math.floor(Math.random() * 1000) + 1;
+    setRandom(random);
+  }, [random]);
+  return <div>Random number:{random}</div>;
 };
 
 export default Random;
